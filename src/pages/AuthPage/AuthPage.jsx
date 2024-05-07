9aff1d806c955eccf3981535a2b64f1e2e448747
Muhammed Zain <muhammedzain2256@gmail.com>
2024-05-07 11:52:51 +0500

    Designed Auth Page

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { Box, Container, VStack, Flex, Image } from "@chakra-ui/react";
+import AuthForm from "../../components/AuthForm/AuthForm";
 
 const AuthPage = () => {
   return (
-    <div>AuthPage</div>
-  )
-}
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}>
+        <Flex justifyContent={"center"} alignItems={"center"} gap={"10"}>
+          {/* Left Hand Side */}
+          <Box display={{ base: "none", md: "block" }}>
+            <Image src="/auth.png" h={650} alt="Phone Img" />
+          </Box>
+          
+          {/* Right Hand Side */}
+          <VStack spacing={4} align={"stretch"}>
+            <AuthForm />
+            <Box textAlign={"center"}>Get the app</Box>
+            <Flex gap={5} justifyContent={"center"}>
+              <Image src="/playstore.png" h={"10"} alt="Playstore Logo" />
+              <Image src="/microsoft.png" h={"10"} alt="Microsoft Logo" />
+            </Flex>
+          </VStack>
+        </Flex>
+      </Container>
+    </Flex>
+  );
+};
 
-export default AuthPage
+export default AuthPage;
